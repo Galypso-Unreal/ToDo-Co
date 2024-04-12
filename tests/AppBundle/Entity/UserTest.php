715bf3b8a97efe242bcf0e15867a31bf859a61dc
Galypso-Unreal <johann.moser0681@gmail.com>
2024-04-12 16:40:45 +0200

    ADD [phpunit] test entity task methods

---
+++ tests/AppBundle/Entity/UserTest.php
@@ -0,0 +1,51 @@
+<?php 
+
+
+namespace Tests\AppBundle\Entity;
+
+use AppBundle\Entity\User;
+use DateTime;
+use PHPUnit\Framework\TestCase;
+
+class UserTest extends TestCase{
+
+    function testId(){
+       /* auto generate id by database untested */
+    }
+
+    function testUsername(){
+        $user = new User();
+        $user->setUsername('John');
+        $this->assertEquals('John',$user->getUsername());
+    }
+
+    function testSalt(){
+        $user = new User();
+        $this->assertEquals(null,$user->getSalt());
+    }
+
+    function testPassword(){
+        $user = new User();
+        $user->setPassword('noencodepass');
+        $this->assertEquals('noencodepass',$user->getPassword());
+    }
+
+    function testMail(){
+        $user = new User();
+        $user->setEmail('email@test.com');
+        $this->assertEquals('email@test.com',$user->getEmail());
+    }
+
+    function testRoles(){
+        $user = new User();
+        $user->setRoles(['ROLE_TEST']);
+        $this->assertEquals(['ROLE_TEST'],$user->getRoles());
+    }
+
+    function testEraseCredentials(){
+        $user = new User();
+        $this->assertEmpty($user->eraseCredentials());
+    }   
+
+
+}

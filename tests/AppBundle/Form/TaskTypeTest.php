77c031dfca5b5d5656c5543811dfd548d4bbda52
Galypso-Unreal <johann.moser0681@gmail.com>
2024-04-20 17:33:07 +0200

    ADD [test] unit-test and coverage

---
+++ tests/AppBundle/Form/TaskTypeTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Tests\AppBundle\Form;
+
+use Symfony\Component\Form\Test\TypeTestCase;
+use AppBundle\Form\TaskType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+
+
+class TaskTypeTest extends TypeTestCase
+{
+    // public function testSubmitValidData()
+    // {
+    //     $formData = [
+    //         'title' => 'Test Title',
+    //         'content' => 'Test Content',
+    //     ];
+
+    //     $form = $this->factory->create(TaskType::class);
+    //     $form->submit($formData);
+
+    //     $this->assertTrue($form->isSynchronized());
+    //     $this->assertEquals($formData['title'], $form->get('title')->getData());
+    //     $this->assertEquals($formData['content'], $form->get('content')->getData());
+    // }
+
+    // Add more test cases as needed
+}

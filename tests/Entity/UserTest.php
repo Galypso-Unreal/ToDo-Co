211092416fc190c02bd239f0fae07cd39a99947f
Galypso-Unreal <johann.moser0681@gmail.com>
2024-05-03 14:48:41 +0200

    UPD [symfony] symfony 4.4.51 installed from scratch project

---
+++ tests/Entity/UserTest.php
@@ -0,0 +1,51 @@
+<?php 
+
+
+namespace App\Tests\Entity;
+
+use App\Entity\User;
+use DateTime;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+class UserTest extends KernelTestCase{
+
+    function testId(){
+       /* auto generate id by database untested */
+    }
+
+    function testUsername(){
+        $user = new User();
+        $user->setUsername('John');
+        $this->assertEquals('John',$user->getUsername());
+    }
+
+    function testSalt(){
+        $user = new User();
+        $this->assertEquals(null,$user->getSalt());
+    }
+
+    function testPassword(){
+        $user = new User();
+        $user->setPassword('noencodepass');
+        $this->assertEquals('noencodepass',$user->getPassword());
+    }
+
+    function testMail(){
+        $user = new User();
+        $user->setEmail('email@test.com');
+        $this->assertEquals('email@test.com',$user->getEmail());
+    }
+
+    function testRoles(){
+        $user = new User();
+        $user->setRoles(['ROLE_TEST']);
+        $this->assertEquals(['ROLE_TEST'],$user->getRoles());
+    }
+
+    function testEraseCredentials(){
+        $user = new User();
+        $this->assertEmpty($user->eraseCredentials());
+    }   
+
+
+}

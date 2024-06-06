1243dc40670e11640fa5d9381e1bd36fbea0aaf8
unknown <johann.moser0681@gmail.com>
2024-06-06 13:00:52 +0200

    UPD [code] space between function test

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -24,6 +24,8 @@ class SecurityController extends AbstractController
      * login form with the last username and any authentication error. If the user is already
      * authenticated, it will redirect to the homepage.
      */
+
+
     public function loginAction(Request $request, AuthenticationUtils $authenticationUtils)
     {
         if ($this->isGranted("IS_AUTHENTICATED_FULLY") === false) {
@@ -50,6 +52,8 @@ class SecurityController extends AbstractController
      *
      * @return void
      */
+
+
     public function loginCheck(): void
     {
         // This code is never executed.
@@ -65,6 +69,8 @@ class SecurityController extends AbstractController
      *
      * @return void
      */
+
+
     public function logoutCheck(): void
     {
         // This code is never executed.

7e43390f55fd9c6ffbe0d015f626b0a282b7a561
unknown <johann.moser0681@gmail.com>
2024-06-06 12:15:53 +0200

    UPD [code] delete pattern end (bug fix)

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -18,6 +18,7 @@ class DefaultController extends AbstractController
     public function index()
     {
         return $this->render('default/index.html.twig');
-    }// End index().
+    }
+    // End index().
 
 }

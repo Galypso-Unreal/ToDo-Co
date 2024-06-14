ae7bcfe43a5c58ee595fdac7c0c7265db468918c
Galypso-Unreal <johann.moser0681@gmail.com>
2024-06-14 16:20:41 +0200

    UPD [code] fix TaskController comment

--- src/Controller/TaskController.php
+++ src/Controller/TaskController.php
@@ -25,7 +25,9 @@ class TaskController extends AbstractController
     /**
      * The function is a constructor that initializes a cache pool object.
      * 
-     * @param CacheItemPoolInterface
+     * @param CacheItemPoolInterface $cachePool this init cache functions like : getitem, remove, ect.
+     * 
+     * @return void function return cache pool init from __construct function.
      */
 
 
@@ -41,9 +43,10 @@ class TaskController extends AbstractController
      * The listAction function retrieves a list of tasks from the cache if available, otherwise fetches
      * them from the database and stores them in the cache.
      * 
-     * @param ManagerRegistry
+     * @param ManagerRegistry $managerRegistry used for use function of managerRegistry like getRepository.
+     * Need to getRepository to queryable tasks.
      * 
-     * @return `listAction` function returns a rendered template 'task/list.html.twig' with an
+     * @return Response `listAction` function returns a rendered template 'task/list.html.twig' with an
      * array of tasks passed as a parameter.
      */
 
@@ -59,6 +62,7 @@ class TaskController extends AbstractController
         } else {
             $tasks = $item->get();
         }
+
         return $this->render('task/list.html.twig', ['tasks' => $tasks]);
 
     }// End listAction().
@@ -69,7 +73,8 @@ class TaskController extends AbstractController
      * This PHP function retrieves a list of tasks marked as done from the cache or database and
      * renders them in a Twig template.
      * 
-     * @param ManagerRegistry
+     * @param ManagerRegistry $managerRegistry used for use function of managerRegistry like getRepository.
+     * Need to getRepository to queryable tasks.
      * 
      * @return `listActionDone` function is returning a rendered template 'task/list.html.twig'
      * with an array of tasks that are marked as done. The tasks are retrieved from the cache if
@@ -99,11 +104,13 @@ class TaskController extends AbstractController
      * The function `createAction` creates a new task, associates it with a user if the user exists,
      * persists it to the database, and redirects to the task list page with a success message.
      * 
-     * @param Request
+     * @param Request $request, request HTTP user for $_POST, $_GETn cookies ect.
+     * This is necessary to get all data from post action.
      * 
-     * @param ManagerRegistry
+     * @param ManagerRegistry $managerRegistry, used to push data to the database.
+     * After summited the form, all data is stocked to the database in table Task.
      * 
-     * @return `createAction` function returns a response that renders the `task/create.html.twig`
+     * @return Response `createAction` function returns a response that renders the `task/create.html.twig`
      * template with the form view if the form has not been submitted or is not valid. If the form is
      * submitted and valid, it persists the task entity, adds the user to the task if the user exists,
      * flushes the entity manager, deletes an item from the cache pool, adds a success flash message,
@@ -149,13 +156,16 @@ class TaskController extends AbstractController
      * This PHP function edits a task if the current user is the owner, otherwise it displays an error
      * message.
      * 
-     * @param Task
+     * @param Task $task, this is the entity Task.
+     * It's used to modify Task entity in database with new data from form.
      * 
-     * @param Request
+     * @param Request $request, request HTTP user for $_POST, $_GETn cookies ect.
+     * This is necessary to get all data from post action.
      * 
-     * @param ManagerRegistry
+     * @param ManagerRegistry $managerRegistry, used to push data to the database.
+     * After summited the form, all data is stocked to the database in table Task.
      * 
-     * @return If the user editing the task is the same as the task's user, the function will return
+     * @return Response the user editing the task is the same as the task's user, the function will return
      * the rendered template for editing the task with the form and task data. If the form is submitted
      * and valid, the task will be updated in the database, a success flash message will be added, and
      * the cache items for task lists will be deleted before redirecting to the task list page.
@@ -188,7 +198,7 @@ class TaskController extends AbstractController
         } else {
             $this->addFlash('error', sprintf('La tâche %s ne peux pas être modifier par un autre utilisateur', $task->getTitle()));
             return $this->redirectToRoute('task_list');
-        }
+        }// End if.
 
     }// End editAction().
 
@@ -198,11 +208,13 @@ class TaskController extends AbstractController
      * The function toggles the status of a task between done and not done, updates the cache, displays
      * a success message, and redirects to the task list page.
      * 
-     * @param Task
+     * @param Task $task, this is the entity Task.
+     * It's used to modify Task entity in database with new data (here get the toggle true/false).
      * 
-     * @param ManagerRegistry
+     * @param ManagerRegistry $managerRegistry, used to push data to the database.
+     * After summited the form, all data is stocked to the database in table Task.
      * 
-     * @return a redirection to the route named 'task_list'.
+     * @return Response redirection to the route named 'task_list'.
      */
 
 
@@ -230,9 +242,11 @@ class TaskController extends AbstractController
      * has admin role and the task belongs to an anonymous user, otherwise it displays an error
      * message.
      * 
-     * @param Task
+     * @param Task $task, this is the entity Task.
+     * It's used to delete Task entity in database.
      * 
-     * @param ManagerRegistry
+     * @param ManagerRegistry $managerRegistry, used to push data to the database.
+     * After summited the form, all data is stocked to the database in table Task.
      * 
      * @return The function `deleteTaskAction` is returning a redirection to the route named
      * 'task_list' after deleting a task if the conditions are met. If the conditions are not met, it

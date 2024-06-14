7a4936e8717cddcb48728854f1107289ea0f4ba5
Galypso-Unreal <johann.moser0681@gmail.com>
2024-06-14 16:42:48 +0200

    UPD [code] fix comment in UserController

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -15,8 +15,7 @@ class UserController extends AbstractController
 {
 
     /**
-     * The line `private CacheItemPoolInterface ;` in the PHP code snippet is declaring a
-     * private property named @var `$cachePool` of type `CacheItemPoolInterface` within the `UserController`
+     * @var `$cachePool` of type `CacheItemPoolInterface` within the `UserController`
      * class.
      */
     private CacheItemPoolInterface $cachePool;
@@ -24,7 +23,7 @@ class UserController extends AbstractController
     /**
      * The function is a constructor that initializes a cache pool object.
      * 
-     * @param CacheItemPoolInterface cachePool The `cachePool` parameter in the constructor is of type
+     * @param CacheItemPoolInterface $cachePool The `cachePool` parameter in the constructor is of type
      * `CacheItemPoolInterface`. This parameter is used to inject an instance of a cache pool
      * implementation into the class. This allows the class to interact with the cache pool to store
      * and retrieve cached data.
@@ -43,9 +42,11 @@ class UserController extends AbstractController
      * The function retrieves a list of users from the cache if available, otherwise fetches it from
      * the database and stores it in the cache.
      * 
-     * @param ManagerRegistry
+     * @param ManagerRegistry $managerRegistry  interface is part of the Doctrine ORM (Object-Relational Mapping) integration.
+     * It provides a way to manage and access the different Doctrine entity managers and connections.
+     * The ManagerRegistry is often used for dependency injection into services or controllers where database operations are required.
      * 
-     * @return The `listAction` function returns a rendered template 'user/list.html.twig' with a
+     * @return Response `listAction` function returns a rendered template 'user/list.html.twig' with a
      * variable 'users' containing the list of users fetched from the cache or database using the
      * ManagerRegistry.
      */
@@ -72,13 +73,15 @@ class UserController extends AbstractController
      * This PHP function creates a new user entity, hashes the user's password, persists the user in
      * the database, and redirects to the user list page if the form submission is successful.
      * 
-     * @param Request
+     * @param Request $request The current HTTP request object, containing query parameters and other request data.
      * 
-     * @param UserPasswordHasherInterface
+     * @param UserPasswordHasherInterface $userPasswordHasher The PasswordHasherInterface service, used to hash the password.
      * 
-     * @param ManagerRegistry
+     * @param ManagerRegistry $managerRegistry  interface is part of the Doctrine ORM (Object-Relational Mapping) integration.
+     * It provides a way to manage and access the different Doctrine entity managers and connections.
+     * The ManagerRegistry is often used for dependency injection into services or controllers where database operations are required.
      * 
-     * @return The `createAction` function returns a response that renders the `user/create.html.twig`
+     * @return Response `createAction` function returns a response that renders the `user/create.html.twig`
      * template with the form view if the form has not been submitted or is not valid. If the form is
      * submitted and valid, it adds the user to the database, flashes a success message, deletes an
      * item from the cache, and redirects to the `user_list` route.
@@ -117,15 +120,17 @@ class UserController extends AbstractController
      * This PHP function edits a user entity, hashes the password, updates the user in the database,
      * and redirects to the user list page with a success message.
      * 
-     * @param User
+     * @param User $user The User entity to be updated with the news datas in form.
      * 
-     * @param Request
+     * @param Request $request The current HTTP request object, containing query parameters and other request data.
      * 
-     * @param UserPasswordHasherInterface
+     * @param UserPasswordHasherInterface $userPasswordHasher The PasswordHasherInterface service, used to hash the password.
      * 
-     * @param ManagerRegistry
+     * @param ManagerRegistry $managerRegistry  interface is part of the Doctrine ORM (Object-Relational Mapping) integration.
+     * It provides a way to manage and access the different Doctrine entity managers and connections.
+     * The ManagerRegistry is often used for dependency injection into services or controllers where database operations are required.
      * 
-     * @return The function `editAction` is returning a rendered template for the `user/edit.html.twig`
+     * @return Response function `editAction` is returning a rendered template for the `user/edit.html.twig`
      * view with the form and user data passed to it. If the form is submitted and valid, it will hash
      * the user's password, update the user entity in the database, add a success flash message, delete
      * an item from the cache, and then redirect to the `user_list` route.
